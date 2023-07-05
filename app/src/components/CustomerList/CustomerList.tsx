2173ceecf9566e73cced01975ca50005ed408d7e
George Luiz 'Maverick' Maluf <georgelmaluf286@gmail.com>
2023-07-04 21:02:24 -0300

    Add ViewCustomer Modal

--- app/src/components/CustomerList/CustomerList.tsx
+++ app/src/components/CustomerList/CustomerList.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { ICustomer } from "../../types/Customer.type";
+import CustomerModal from "../CustomerModal/CustomerModal";
 import "./CustomerList.style.css"
 
 type Props = {
@@ -8,36 +10,52 @@ type Props = {
 
 const CustomerList = (props: Props) => {
   const { list, onDeleteClickHnd } = props;
+  const [showCustomerModal, setShowCustomerModal] = useState(false);
+  const [currentCustomer, setCurrentCustomer] = useState(null as ICustomer | null);
+
+  const onDetailClick = (data: ICustomer) => {
+    setCurrentCustomer(data);
+    setShowCustomerModal(true)
+  }
+
+  const onCloseModalClick = () => {
+    setShowCustomerModal(false)
+  }
 
   return(
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Kind</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-      {list.map(customer => {
-        return (
-          <tr key={customer.id}>
-            <td>{customer.name}</td>
-            <td>{customer.email}</td>
-            <td>{customer.kind}</td>
-            <td>
-              <div>
-                <input type="button" value="Details" />
-                <input type="button" value="Edit" />
-                <input type="button" value="Delete" onClick={() => onDeleteClickHnd(customer)} />
-              </div>
-            </td>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Kind</th>
+            <th></th>
           </tr>
-        )
-      })}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        {list.map(customer => {
+          return (
+            <tr key={customer.id}>
+              <td>{customer.name}</td>
+              <td>{customer.email}</td>
+              <td>{customer.kind}</td>
+              <td>
+                <div>
+                  <input type="button" value="Details" onClick={() => onDetailClick(customer)}  />
+                  <input type="button" value="Edit" />
+                  <input type="button" value="Delete" onClick={() => onDeleteClickHnd(customer)} />
+                </div>
+              </td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+      { showCustomerModal && currentCustomer != null && (
+        <CustomerModal onCloseClickHnd={onCloseModalClick} customer={currentCustomer}/>
+      )}
+    </div>
   );
 }
 
---
+++ app/src/components/CustomerModal/CustomerModal.tsx
@@ -0,0 +1,35 @@
+import { ICustomer } from "../../types/Customer.type";
+import "./CustomerModal.style.css"
+
+type Props = {
+  onCloseClickHnd: () => void
+  customer: ICustomer;
+}
+
+const CustomerModal = (props: Props) => {
+  const { onCloseClickHnd, customer } = props;
+
+  return (
+    <div id="myModal" className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onCloseClickHnd}>&times;</span>
+        <div>
+          <h3>Customer Details</h3>
+          <div>
+            <div>
+              <label>Name : {customer.name}</label>
+            </div>
+            <div>
+              <label>Email : {customer.email} </label>
+            </div>
+            <div>
+              <label>Kind : {customer.kind}</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default CustomerModal;
