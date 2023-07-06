74fee4d277f92cbbd8680532aa42251497432bed
George Luiz 'Maverick' Maluf <georgelmaluf286@gmail.com>
2023-07-05 23:06:38 -0300

    feat: Add show method for CustomerController

--- back/app/controllers/api/v1/customers_controller.rb
+++ back/app/controllers/api/v1/customers_controller.rb
@@ -3,9 +3,24 @@
 module Api
   module V1
     class CustomersController < ApplicationController
+      before_action :set_customer, only: %i[show]
+
       def index
         @customers = CustomersServices.new.list_all
         render json: @customers, status: :ok
+      end
+
+      def show
+        if @customer.blank?
+          render json: { message: 'Customer not found' }, status: :unprocessable_entity
+        else
+          render json: @customer, status: :ok
+        end
+      end
+
+      private
+      def set_customer
+        @customer = Customer.find(params[:id])
       end
     end
   end
--- back/spec/requests/api/v1/customers_spec.rb
+++ back/spec/requests/api/v1/customers_spec.rb
@@ -3,10 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Customers", type: :request do
-  describe "GET /index" do
+  describe "#index" do
     it 'returns http success' do
       get api_v1_customers_path
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "#show" do
+    let(:customer) { create(:customer) }
+
+    it 'when exists customer id' do
+      aggregate_failures do
+        get api_v1_customer_path(id: customer.id)
+        expect(response).to have_http_status(:success)
+        expect(response.body).to eq(CustomerSerializer.new(customer).to_json)
+      end
     end
   end
 end
